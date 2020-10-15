62376d530ef5bfc8b32ff77f0f36f6c5dca99167
Velythyl <charlie.gauthier@umontreal.ca>
2020-10-15 15:47:46 -0400

    obj detection example

--- test_segmentation.py
+++ test_segmentation.py
@@ -4,6 +4,21 @@ import random
 
 import numpy as np
 
+# Question 1: investigation:
+
+import cv2
+
+img = cv2.imread("./PennFudanPed/PNGImages/FudanPed00001.png", cv2.IMREAD_COLOR)
+cv2.imshow("image", img)
+cv2.waitKey(0)
+
+mask = cv2.imread("./PennFudanPed/PedMasks/FudanPed00001_mask.png", cv2.IMREAD_UNCHANGED)
+mask = np.floor(mask / np.max(mask) * 255).astype(np.uint8)
+cv2.imshow("image", cv2.applyColorMap(mask, cv2.COLORMAP_RAINBOW))
+cv2.waitKey(0)
+
+# Question 2: data gathering
+
 from gym_duckietown.envs import DuckietownEnv
 from gym_duckietown.simulator import AGENT_SAFETY_RAD
 
@@ -11,7 +26,6 @@ POSITION_THRESHOLD = 0.04
 REF_VELOCITY = 0.7
 FOLLOWING_DISTANCE = 0.24
 AGENT_SAFETY_GAIN = 1.15
-
 
 class PurePursuitPolicy:
     """
@@ -190,3 +204,14 @@ while True:
     print("mean episode reward:", np.mean(rewards))
 
 environment.close()
+
+
+# Question 3: data annotation & cleaning
+
+# todo: remove all non-duckies from the masks by removing all non-duckie pixels. Also, each blob must be a different value
+# use the function from question 1 that maps masks with single-digit pixel values to visual masks to debug
+
+# Question 4: object detection NN
+
+# https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
+# Or maybe just train on finding the centroid of the blobs, and assume all objects will be 50x50 or something like that
